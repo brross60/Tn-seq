e233d37606fc1478ce48e8f641e3eb3bae2a327c
WhiteleyLab <dannyc@anche.no>
2020-08-20 11:34:36 -0400

    Update count_TA.R
    By counting both the TA and the AT sites, we were double counting possible sites. Fixed this issue to only count TA sites.

--- count_TA.R
+++ count_TA.R
@@ -10,11 +10,12 @@ genome <- readDNAStringSet("Bcenocepacia_J2315_modJV_cat.fa")
 
 #Match TA pattern
 ta_sites <- vmatchPattern("TA", genome)
-at_sites <- vmatchPattern("AT", genome)
+#at_sites <- vmatchPattern("AT", genome)
+  #Just read the TA sites, reading both will double count
 
 #Make a single sorted vector of all locations
 ta_vect <- unname(unlist(start(ta_sites)))
-at_vect <- unname(unlist(start(at_sites)))
-all_vect <- sort(c(ta_vect, at_vect))
-ta_df <- data_frame(sites = all_vect)
+#at_vect <- unname(unlist(start(at_sites)))
+#all_vect <- sort(c(ta_vect, at_vect))
+ta_df <- data_frame(sites = ta_vect)
 write_tsv(ta_df, "Bcen_TAsites.tsv", col_names = TRUE)
